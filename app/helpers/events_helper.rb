614a41027fda76830c842f29bd210918dd12500d
tokhi <shafi.tokhi@gmail.com>
2013-04-25 19:49:13 +0430

    protocol label

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+
+	def protocol_label(event)
+		if !event.protocol.nil? && event.protocol.length > 20
+			content_tag(:span, 'Show', :class => "label label-success")
+		else
+			content_tag(:span, 'Show', :class => "label")
+		end
+
+	end
 end
